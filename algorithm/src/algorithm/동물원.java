e404e961ad71e4ba02c5ead5ec4783a130d420a3
BEP <browneyedprogrammer@gmail.com>
2018-10-28 18:40:50 +0900

    동물원

---
+++ algorithm/src/algorithm/동물원.java
@@ -0,0 +1,13 @@
+package algorithm;
+
+import java.io.*;
+
+public class 동물원 {
+	static int N;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		
+	}
+
+}
